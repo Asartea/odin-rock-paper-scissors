fd659550995f522f9384d09fd133ae6d973bf4b6
Asartea <76259120+Asartea@users.noreply.github.com>
2022-11-04 20:53:20 +0100

    start and finish

---
+++ rock-paper-scissors.js
@@ -0,0 +1,44 @@
+let computerScore = 0
+let playerScore = 0
+// get random computer choice
+function getComputerChoice() {
+    let options = ['Rock', 'Paper', 'Scissors']
+    let choice = options[~~(Math.random() * options.length)]
+    return choice
+}
+//get player choice
+function getPlayerChoice() {
+    let choice = prompt('What do you choose?', 'Rock')
+    return choice
+}
+function playRound() {
+    let computerChoice = getComputerChoice().toLowerCase()
+    let playerChoice = getPlayerChoice().toLowerCase()
+    if (
+        (computerChoice == "rock" && playerChoice == "scissors") ||
+        (computerChoice == "paper" && playerChoice == "rock") ||
+        (computerChoice == "scissors" && playerChoice == "paper")
+    ) {
+        console.log("Computer won")
+        computerScore = ++computerScore
+    } else if (
+        (playerChoice == "rock" && computerChoice == "scissors") ||
+        (playerChoice == "paper" && computerChoice == "rock") ||
+        (playerChoice == "scissors" && computerChoice == "paper")
+    ) {
+        console.log("Human won")
+        playerScore = ++playerScore
+    } else {
+        console.log("tie")
+    }
+}
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound()
+    }
+    if (computerScore > playerScore) {
+        console.log("Computer won the Game")
+    } else {
+        console.log("Human won the Game")
+    }
+}
